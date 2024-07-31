53e27b9a094e4b6c70d5eaa9f838efec0a879eb1
peacegreat <nanbyenlazwan@gmail.com>
2024-07-31 11:57:42 +0100

    nav-bar

--- src/components/FoodItem/FoodItem.js
+++ src/components/FoodItem/FoodItem.js
@@ -10,7 +10,10 @@ const FoodItem = ({id,name,price,description,image}) => {
   return (
     <div className='food-item'>
         <div className='food-item-img-container'> 
-            <img className='food-item-image' src={image} alt=''/>            
+            <img className='food-item-image' src={image} alt=''/> 
+            <button>
+                <p>ADD TO CART</p>
+            </button>           
             {!cartItems[id]
                 ? (<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt=''/>)
                 :(<div className='food-item-counter'>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,15 +12,15 @@ function NavBar({setShowLogin}) {
     <div className='navbar'>
         <Link to="/"><img src={assets.logo} alt='' className='logo'/></Link>
         <ul className='navbar-menu'>
-            <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-            <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-            <a href="#app-download"onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-            <a href="#footer" onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>            
+            <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>HOME</Link>
+            <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>MENU</a>
+            <a href="#app-download"onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>MOBILE-APP</a>
+            <a href="#footer" onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>CONTACT US</a>            
         </ul>
         <div className='navbar-right'>
             <img src={assets.search_icon} alt=''/>
         <div className='navbar-search-icon'>
-            <Link to="/cart"><img src={assets.basket_icon}/></Link>
+            <Link to="/cart"><img src={assets.basket_icon} alt=''/></Link>
         <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>sign up</button>
